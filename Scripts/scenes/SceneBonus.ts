19b57da2f064b3857b894aa995cc40983a527b5e
JViniX <47263194+JViniX@users.noreply.github.com>
2020-02-22 20:48:03 -0500

    Scene Bonus updated

--- Scripts/scenes/SceneBonus.ts
+++ Scripts/scenes/SceneBonus.ts
@@ -40,10 +40,6 @@ module scenes
         {
              // Defining the table background.
             this._table = new objects.Image(config.Game.ASSETS.getResult("table"), "table", 0, 0, false);
-            //this._table.width = 640;
-            //this._table.height = 580;
-            // this._table.scaleX = this._table.width;
-            // this._table.scaleY = this._table.height;
 
             // buttons
             this._rollButton = new objects.Button(config.Game.ASSETS.getResult("rollButton"), 320, 550, true);
